abc050584c638b66728af4894abbd2f6a76eff53
AceNova287 <tom123harry123@gmail.com>
2025-08-20 17:55:13 +0530

    ACP1

--- Lesson 1/main.py
+++ Lesson 1/main.py
@@ -1,3 +1,13 @@
-git config --global user.name "AceNova287"
+print("Hi, Welcome to ChatBot!")
+name = input("What's your name? ")
+print(f"Hello {name}, Nice to meet you")
+print("How are you feeling today? (good/bad) : ")
+mood = input().lower()
+if mood == "good" or mood == "Good":
+    print("That's great to hear!")
+elif mood == "bad" or mood == "Bad":
+    print("I'm sorry to hear that.")
+else:
+    print("Sometimes its hard to express your feeling.")
 
-git config --global user.email "tom123harry123@gmail.com"
+print(f"It was nice chatting with you {name}. Goodbye!")
